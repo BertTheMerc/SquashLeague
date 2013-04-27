a230ddcdba931f771ea7c80ea4a7ca28bed5d226
John Hall <BertTheMerc@gmail.com>
2013-04-27 09:59:48 +0100

    Init checkin

---
+++ SquashLeague/Controllers/LeagueTableController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SquashLegaue.Controllers
+{   
+    using SquashLegaue.Models;
+
+    public class LeagueTableController : Controller
+    {
+        private List<LeagueTable> table = new List<LeagueTable>();
+        private int _nextId = 1;
+
+        SqlConnection con;
+        SqlDataAdapter da;
+        DataSet ds = new DataSet();
+ 
+        public ActionResult Index()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+            da = new SqlDataAdapter("select * from vLeagueTable ORDER BY POINTS DESC, GamesDiff DESC", con);
+            da.Fill(ds);
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                table.Add(new LeagueTable()
+                {
+                    Name = dr["Name"].ToString(),
+                    Matchs = int.Parse(dr["Matchs"].ToString()),
+                    Win = int.Parse(dr["Win"].ToString()),
+                    Draw = int.Parse(dr["Draw"].ToString()),
+                    Lost = int.Parse(dr["Lost"].ToString()),
+                    GamesWon = int.Parse(dr["GamesWon"].ToString()),
+                    GamesLost = int.Parse(dr["GamesLost"].ToString()),
+                    GamesDiff = int.Parse(dr["GamesDiff"].ToString()),
+                    Points = int.Parse(dr["Points"].ToString()),
+                    WinPercentage = int.Parse(dr["WinPercentage"].ToString())
+                } );
+            }          
+            return View(table);
+        }
+
+    }
+}
---
+++ SquashLeague/Models/LeagueTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SquashLegaue.Models
+{
+    public class LeagueTable
+    {
+        [Display(Name = "Player")]
+        public string Name {get; set;}
+
+        [Display(Name = "Matchs played")]
+        public int Matchs { get; set; }
+
+        [Display(Name = "Wins")]
+        public int Win { get; set; }
+
+        [Display(Name = "Draws")]
+        public int Draw { get; set; }
+
+        [Display(Name = "Losess")]
+        public int Lost { get; set; }
+
+        [Display(Name = "Games Won")]
+        public int GamesWon { get; set; }
+
+        [Display(Name = "Games Lost")]
+        public int GamesLost { get; set; }
+
+        [Display(Name = "Games +/-")]
+        public int GamesDiff { get; set; }
+
+        [Display(Name = "Ponits")]
+        public int Points { get; set; }
+
+        [Display(Name = "Win%")]
+        public int WinPercentage { get; set; }
+    }
+}
